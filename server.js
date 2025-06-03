3141093a09adba447b5ca11346b8da65b5ef91fc
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-06-03 11:25:12 -0700

    get all todos

--- server.js
+++ server.js
@@ -1,15 +1,51 @@
 const http = require("http");
-
+const fs = require("fs");
+const path = require("path"); 
+  
+const filepath = path.join(__dirname, "./db/todo.json");
 
 const server = http.createServer((req, res) =>{
     // res.end("Well come to todo App")
 
     if(req.url === "/todos" && req.method === "GET"){
-        res.send("All Todos")
+     
+        const data = fs.readFileSync(filepath, {encoding: 'utf-8'})
+        res.writeHead(201, {
+            "content-type" : "application/json",
+           
+        })
+        res.end(data);
+
+
     } else if (req.url === "/todos/create-todo" && req.method === "POST") {
-        res.send("Todo created") 
+     
+        let data = ""
+
+        req.on("data", (chank)=>{
+            data = data + chank ;
+        })
+
+        req.on("end", ()=>{
+            console.log(data);
+            
+            const {title, body} = JSON.parse(data);
+            console.log({title, body});
+
+            const createdAt = new Date().toLocaleString()
+            const allTodos = fs.readFileSync(filepath, {encoding: "utf-8"})
+
+            const parseAllTodos = JSON.parse(allTodos);
+
+            parseAllTodos.push({title, body, createdAt})
+            fs.writeFileSync(filepath, JSON.stringify(parseAllTodos, null, 2), {encoding: "utf-8"})
+
+        res.end(JSON.stringify({title, body, createdAt}, null, 2)) 
+
+        })
+            
+
     } else{
-        res.send("Route Not found !")
+        res.end("Route Not found !")
     }
 })
 
