97023725927c7c0344c9f3171018f4772af75b1c
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-06-05 09:54:55 -0700

    create a new file

--- Express/src/server.ts
+++ Express/src/server.ts
@@ -3,7 +3,7 @@ import app from "./app";
  let server ;
 const port = 5000
 
-
+  /* create a bootsrap */
  const bootstrap = async () =>{
     server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
