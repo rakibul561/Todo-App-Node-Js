19ae24a8efc9160c7ff576ddab9f45e2f9c929d7
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-06-06 10:33:25 -0700

    updated code

--- Express/src/app.ts
+++ Express/src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application, Request, Response } from 'express'
 const app:Application = express()
-
+ 
+// get new todos
 app.get('/', (req:Request, res: Response) => {
   res.send('wellcome to Todos App !!!')
 })
+// get all todos
 app.get('/todos', (req:Request, res: Response) => {
   res.send('Hello World!')
 })
