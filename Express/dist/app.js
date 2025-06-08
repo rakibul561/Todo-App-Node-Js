615ffe4e4cca5fabb6f78ce231cf113973f71676
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-06-08 09:36:03 -0700

    create a todos post

--- Express/dist/app.js
+++ Express/dist/app.js
@@ -7,17 +7,21 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-const filepath = path_1.default.join(__dirname, "./db/todo.json");
+app.use(express_1.default.json());
+const filepath = path_1.default.join(__dirname, "../db/todo.json");
 // get new todos
 app.get('/', (req, res) => {
-    res.send('wellcome to Todos App !!!');
+    res.send('wellcome to  Todos App  f!!!');
 });
 // get all todos
 app.get('/todos', (req, res) => {
     const data = fs_1.default.readFileSync(filepath, { encoding: 'utf-8' });
-    res.send(data);
+    console.log(data);
+    res.json(data);
 });
 app.post('/todos/create-todo', (req, res) => {
+    const { title, body } = req.body;
+    console.log(title, body);
     res.send('Hello World!');
 });
 exports.default = app;
--- Express/src/app.ts
+++ Express/src/app.ts
@@ -2,24 +2,30 @@ import express, { Application, Request, Response } from 'express'
 const app:Application = express()
 import fs from 'fs';
 import path from 'path'
-const filepath = path.join(__dirname ,"../express/db/todo.json");
-// const filepath = path.join(__dirname ,"../express/db/todo.json");
-// const filepath = path.join(__dirname ,"../express/db/todo.json");
 
 
+app.use(express.json());
+
+
+const filepath = path.join(__dirname ,"../db/todo.json");
  
 // get new todos
 app.get('/', (req:Request, res: Response) => {
-  res.send('wellcome to Todos App !!!')
+  res.send('wellcome to  Todos App  f!!!')
 })
 // get all todos
 app.get('/todos', (req:Request, res: Response) => {
 
-    const data = fs.readFileSync(filepath, {encoding: 'utf-8'})
+    const data = fs.readFileSync(filepath, {encoding: 'utf-8'});
+    console.log(data);
+    
        
-  res.send(data)
+  res.json(data)
 })
 app.post('/todos/create-todo', (req:Request, res: Response) => {
+  const {title, body} = req.body ;
+  console.log(title, body);
+  
   res.send('Hello World!')
 })
 
