e5ec9f8cab32a33439678168d93098aa63b47b94
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-06-10 22:10:44 -0700

    todos added database

--- Express/dist/app/todos/todos.Routes.js
+++ Express/dist/app/todos/todos.Routes.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,23 +15,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.todoRouter = void 0;
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
+const mongodb_1 = require("../../config/mongodb");
 const filepath = path_1.default.join(__dirname, "../../../db/todo.json");
 exports.todoRouter = express_1.default.Router();
-exports.todoRouter.get('/', (req, res) => {
-    const data = fs_1.default.readFileSync(filepath, { encoding: 'utf-8' });
-    console.log("From todos Router");
-    res.json({
-        message: "From todos router",
-        data
+exports.todoRouter.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const db = yield mongodb_1.client.db("todosDB");
+    const collection = yield db.collection('todos');
+    const cursor = collection.find({});
+    const todos = yield cursor.toArray();
+    res.json(todos);
+}));
+exports.todoRouter.post('/create-todo', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { title, priority, description } = req.body;
+    const db = yield mongodb_1.client.db("todosDB");
+    const collection = yield db.collection('todos');
+    yield collection.insertOne({
+        title: title,
+        description: description,
+        priorit: priority,
+        isCompleted: false
     });
-});
-exports.todoRouter.post('/create-todo', (req, res) => {
-    const { title, body } = req.body;
-    console.log(title, body);
-    res.send('Hello World!');
-});
+    const cursor = collection.find({});
+    const todos = yield cursor.toArray();
+    res.send(todos);
+}));
 exports.todoRouter.get('/:title', (req, res) => {
     const { title, body } = req.body;
     console.log(title, body);
--- Express/src/app/todos/todos.Routes.ts
+++ Express/src/app/todos/todos.Routes.ts
@@ -3,6 +3,7 @@ import express, { Application, Request, Response } from 'express'
 const app:Application = express()
 import fs from 'fs';
 import path, { join } from 'path'
+import { client } from '../../config/mongodb';
 
 const filepath = path.join(__dirname ,"../../../db/todo.json");
 
@@ -12,22 +13,45 @@ const filepath = path.join(__dirname ,"../../../db/todo.json");
 
 
 
-todoRouter.get('/',(req:Request, res:Response) =>{
-    const data = fs.readFileSync(filepath, {encoding: 'utf-8'});
-    console.log("From todos Router");
+todoRouter.get('/', async (req:Request, res:Response) =>{
     
-  res.json({
-    message: "From todos router",
-    data
-  })
+    const db = await client.db("todosDB")
+    const collection = await db.collection('todos')
+ 
+ const cursor =  collection.find({})
+ const todos = await cursor.toArray();
+  res.json(todos)
+
 } )
 
-todoRouter.post('/create-todo', (req:Request, res: Response) => {
-  const {title, body} = req.body ;
-  console.log(title, body);
-  
-  res.send('Hello World!')
+
+
+todoRouter.post('/create-todo',async (req:Request, res: Response) => {
+
+  const {title,priority, description} = req.body ;
+
+
+    const db = await client.db("todosDB")
+    const collection = await db.collection('todos')
+  await collection.insertOne({
+  title: title,
+  description: description,
+  priorit:priority,
+  isCompleted:false
 })
+
+ const cursor =  collection.find({})
+ const todos = await cursor.toArray();
+
+
+  
+
+
+  
+  res.send(todos)
+})
+
+
 
 todoRouter.get('/:title', (req:Request, res: Response) => {
   const {title, body} = req.body ;
